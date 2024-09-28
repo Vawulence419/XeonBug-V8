340f9a205085ce6cd9fe8c8fd9433be89cf4049f
Vawulence419 <vawulence994@gmail.com>
2024-09-28 20:32:20 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "2349030359124"
+global.ownername = "Vawulence"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '2349030359124'  //creator number
+global.ownername = 'Vawulence' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
